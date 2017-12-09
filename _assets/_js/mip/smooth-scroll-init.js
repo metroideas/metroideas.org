9851e8156d50a15f617b71afa64dbb6206c65f35
David Morton <michaeldavidmorton@gmail.com>
2017-12-09 11:18:36 -0500

    Update smooth-scroll init

--- _assets/_js/mip/smooth-scroll-init.js
+++ _assets/_js/mip/smooth-scroll-init.js
@@ -1,6 +1,6 @@
 window.mip = window.mip || {};
 var mip = window.mip;
-var smoothScroll = require('smooth-scroll');
+var SmoothScroll = require('smooth-scroll');
 
 (function($) {
   // Remove html root's 'no-js' class
@@ -8,12 +8,20 @@ var smoothScroll = require('smooth-scroll');
   $.removeClass(html, 'no-js');
   
   // Initialize smooth scrolling
-  smoothScroll.init({
-    selector: '[data-scroll]',
-    selectorHeader: null,
-    speed: 500,
-    easing: 'easeInOutCubic',
-    offset: 0,
-    callback: function ( anchor, toggle ) {}
+  new SmoothScroll('a[href*="#"]', {
+    // Selectors
+    ignore: '[data-scroll-ignore]', // Selector for links to ignore (must be a valid CSS selector)
+    header: null, // Selector for fixed headers (must be a valid CSS selector)
+
+    // Speed & Easing
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+    easing: 'easeInOutCubic', // Easing pattern to use
+    customEasing: function (time) {}, // Function. Custom easing pattern
+
+    // Callback API
+    before: function () {}, // Callback to run before scroll
+    after: function () {} // Callback to run after scroll
   });
 })(mip);
+
